9af3e2466241cab02ab02e24c284edca4047a93a
yunnus <122170438+yunnus143@users.noreply.github.com>
2023-05-18 19:50:12 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,5 +8,6 @@ app.use(express.json());
 
 app.use('/api/v1/', router);
 // add global error handling middleware defined in middlewares/errorHandler.js
+app.use(errorHandler);
 
 module.exports = app;
